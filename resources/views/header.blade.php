12aaec5c67fa12b24b8be172d472822e5260d856
RockDrive <m-shamil-92@yandex.ru>
2022-02-20 04:50:02 +0300

    Корректировки

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -8,3 +8,9 @@
           rel="icon"
     >
 @endpush
+
+<div class="h2 fw-light d-flex align-items-center">
+    <p class="ms-3 my-0 d-none d-sm-block">
+        {{ config('app.name') }}
+    </p>
+</div>
